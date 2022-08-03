78c7f28b0520aae50419f9a7e0f45eddc976287a
RedaMeziouni <meziounidb@gmail.com>
2022-08-03 17:08:19 +0100

    SolveBoard methode that use recursion

--- JavaSolution/SudokuSolver.java
+++ JavaSolution/SudokuSolver.java
@@ -60,5 +60,26 @@ public class SudokuSolver {
         return !isNumberInRow(board, number, row) && !isNumberInColumn(board, number, column) && !isNumberInBox(board, number row, column);
     }
 
-    
+    private static boolean solveBoard(int[][] board) {
+        for(int row = 0; row < GRID_SIZE; row++) {
+            for(int column = 0; column < GRID_SIZE; column++) {
+                if(board[row][column] == 0) {
+                    for(int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+                        if(isValidPlacement(board, numberToTry, row, column)) {
+                            board[row][column] = numberToTry;
+
+                            // Recursion
+                            if(solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
