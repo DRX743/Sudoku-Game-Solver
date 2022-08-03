9b27ce7c7d15ff25db69a8b2888994e157cf3f0e
RedaMeziouni <meziounidb@gmail.com>
2022-08-03 15:31:17 +0100

    Implement the validation checks logic"

--- PythonSolution/sudoku.py
+++ PythonSolution/sudoku.py
@@ -13,6 +13,39 @@ def find_next_empty(puzzle);
 
     return None, None  # if no spaces in the puzzle are empty (-1)
 
+def is_valid(puzzle, guess, row, col):
+    # figures out whether the guess at the row/col of the puzzle is a valid guess
+    # returns True or False
+
+    # for a guess to be valid, then we need to follow the sudoku rules
+    # that number must not be repeated in the row, column, or 3x3 square that it appears in
+
+    # Row
+    row_vals = puzzle[row]
+    if guess in row_vals:
+        return False # if we've repeated, then our guess is not valid!
+
+    # Column
+    # col_vals = []
+    # for i in range(9):
+    #     col_vals.append(puzzle[i][col])
+    col_vals = [puzzle[i][col] for i in range(9)]
+    if guess in col_vals:
+        return False
+
+    # and then the 3x3 square
+    row_start = (row // 3) * 3 # 10 // 3 = 3, 5 // 3 = 1, 1 // 3 = 0
+    col_start = (col // 3) * 3 # Exact same logic for columns
+
+    for r in range(row_start, row_start + 3):
+        for c in range(col_start, col_start + 3):
+            if puzzle[r][c] == guess:
+                return False
+
+    # If we get at thhis place, these checks pass => Validate
+    return True
+
+
 def solve_sudoku(puzzle);
 
     # 1st step : choose a cell to make a guess
@@ -21,3 +54,14 @@ def solve_sudoku(puzzle);
     # step 2: if there's nowhere left, then we're done because we only allowed valid inputs
     if row is None:  # this is true if our find_next_empty function returns None, None
         return True 
+
+    # step 3: if there is a place to put a number, then make a guess between 1 and 9
+    # range(1, 10) is 1, 2, 3, ... 9
+    for guess in range(1, 10): 
+        # step 4: check if this is a valid guess
+        if is_valid(puzzle, guess, row, col):
+            # step 5: if this is a valid guess, then place it at that spot on the puzzle
+            puzzle[row][col] = guess
+            # step 6: then we recursively call our solver!
+            if solve_sudoku(puzzle):
+                return True
