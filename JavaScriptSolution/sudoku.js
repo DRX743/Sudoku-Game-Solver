3857349e29ee89915735e76b10d512b0a9ee470f
RedaMeziouni <meziounidb@gmail.com>
2022-08-03 15:58:32 +0100

    Make the search for solution function that use the backtracking

--- JavaScriptSolution/sudoku.js
+++ JavaScriptSolution/sudoku.js
@@ -10,3 +10,22 @@ function solver(board) {
         return searchForSolution(validBoards) // helper function that will use backtracking
     }
 }
+
+function searchForSolution(boards){
+    // List[Board] -> Board or false
+    // finds a valid solution to the sudoku problem
+    if (boards.length < 1){
+        return false
+    }
+    else {
+        // backtracking search for solution
+        var first = boards.shift() //take the 1st element Off
+        const tryPath = solve(first)
+        if (tryPath != false){
+            return tryPath
+        }
+        else{
+            return searchForSolution(boards)
+        }
+    }
+}
